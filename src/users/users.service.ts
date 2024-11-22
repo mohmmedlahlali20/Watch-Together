85f43f4d509b86ac079c78c5d433bfceabf5f288
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 21:58:20 +0100

    create features users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+
+  async GetAllUsers(): Promise<UserDocument[]> {
+    return await this.userModel.find().exec();
+  }
+}
