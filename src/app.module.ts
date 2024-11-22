6861ade6a9c235b6c92d0d4d80f2352483204dae
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 05:54:30 +0100

    import AuthModul in App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 'mongodb://localhost:27017/watchToGetherDb',
+    ),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
