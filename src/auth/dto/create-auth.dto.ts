e5fd51302edd780603c8a1ea6380bfd612d0b089
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 05:57:34 +0100

    create validation auth in dto

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+}
