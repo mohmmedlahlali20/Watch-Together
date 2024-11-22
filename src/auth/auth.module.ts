84de347694a3420ea57874ef1ce57340990cfab4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-22 05:55:54 +0100

    fix dontenv .env , create module jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/schemas/user.schema';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
-  controllers: [AuthController],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [MongooseModule],
 })
 export class AuthModule {}
