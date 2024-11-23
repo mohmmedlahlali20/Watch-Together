74caa11670b8b405bc411bb2a90b7080a3632f65
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-23 16:04:56 +0100

    Create endpoints get all video by room ID

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Res, UseGuards } from "@nestjs/common";
 import { RoomService } from './room.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { Room } from './schemas/room.schema';
+import { ParticipantGuard } from "../guards/participant.guard";
+import { JwtGuard } from "../guards/jwt.guard";
 
 @Controller('rooms')
+@UseGuards(JwtGuard)
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
@@ -39,6 +42,54 @@ export class RoomController {
     } catch (error) {
       console.error('Error fetching videos for room:', error.message);
       return { message: 'Failed to fetch videos', error: error.message };
+    }
+  }
+
+  @Get(':roomId/playlist')
+  @UseGuards(ParticipantGuard)
+  async getPlaylist(@Param('roomId') roomId: string) {
+    try {
+      const playlist = await this.roomService.getVideosByRoomId(roomId);
+
+      if (!playlist || playlist.length === 0) {
+        return { message: 'No playlist found for the specified room', playlist: [] };
+      }
+
+      return { message: 'Playlist retrieved successfully', playlist };
+    } catch (error) {
+      console.error('Error fetching playlist for room:', error.message);
+      return {
+        message: 'Failed to fetch playlist',
+        error: error.message,
+      };
+    }
+  }
+
+  @Get('/GetRoomById/:roomId')
+  async getRoomByIdParticipants(@Param('roomId') roomId: string) {
+    try {
+      const participants =
+        await this.roomService.getRoomByIdParticipants(roomId);
+
+      if (!participants || participants.length === 0) {
+        return { message: 'No participants found for this room' };
+      }
+
+      return { message: 'Participants retrieved successfully', participants };
+    } catch (error) {
+      console.error('Error fetching room participants:', error.message);
+      return { message: 'Failed to fetch participants', error: error.message };
+    }
+  }
+
+  @Get(':roomId')
+  async getOneRoom(@Param('roomId') roomId: string){
+    try {
+      const room = await this.roomService.findOneById(roomId);
+      console.log(room);
+      return {message: 'Room found', room}
+    }catch (e) {
+      console.log('err getting');
     }
   }
 }
