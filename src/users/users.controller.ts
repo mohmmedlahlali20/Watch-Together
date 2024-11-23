1294086388cbd62133402c9a0eed55dac479fe1b
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-23 01:23:10 +0100

    Fix: fix  router

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
+import { UsersService } from './users.service';
+import { Controller, Get } from '@nestjs/common';
 
 
+
+
+@Controller('User')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get('Users')
+  async getAllUsers() {
+    return this.userService.GetAllUsers();
+  }
 }
